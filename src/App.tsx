f26a0ad8329fab2022f39f3b5751b3d41c8521f1
karolyangqian <karolyangqian14@gmail.com>
2022-01-29 01:16:45 +0700

    image with counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import arthur from './img/arthur.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    
+    
+  }
+  render() {
+    return (
+      <div>
+        <Info/>
+        <Counter/>
+      </div>
+      );     
+    }
+  }
+  
+class Info extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nama: "Karol",
+      kelas:"XI MIPA 1",
+      waktu:"pagi"
+  };
+  }
+  render() {
+    return (
+
+      <div className="App">
+        <h1>Nama: {this.state.nama}</h1>
+        <h2 style={{fontWeight: 'normal'}}>Kelas: {this.state.kelas}</h2>
+        <h2 style={{fontWeight: 'normal'}}>Pagi: {this.state.waktu=="pagi" ? "true" : "false"}</h2>
+
+      </div>
+
+
+    );     
+  }
 }
 
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  add(x) {
+    this.setState(state => ({
+      count: state.count + x
+    }));
+  }
+  render(): React.ReactNode {
+      return (
+        <div className='App'>
+          <h1>{this.state.count}</h1>
+          <div className='button-wrapper'>
+            <button onClick={()=>this.add(-1)}>-</button>
+            <button onClick={()=>this.add(1)}>+</button>
+          </div>
+          <Image count={this.state.count}/>
+
+        </div>
+      );
+  }
+}
+
+ class Image extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(): React.ReactNode {
+    let image;
+    if(this.props.count%3===0){
+      image = <img src={arthur} alt="foto arthur" />;
+    } else {
+      image = null;
+    }
+      return (
+        <div>
+          {image}
+        </div>
+      );
+  }
+}
 export default App;
